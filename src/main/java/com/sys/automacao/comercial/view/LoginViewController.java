874c41d60997982819c61e2f840d7f71deb967cb
falcao998 <felipesouza_analista@yahoo.com.br>
2018-07-14 16:43:01 -0300

    JPA

--- src/main/java/com/sys/automacao/comercial/view/LoginViewController.java
+++ src/main/java/com/sys/automacao/comercial/view/LoginViewController.java
@@ -3,6 +3,8 @@ package com.sys.automacao.comercial.view;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
 import com.sys.automacao.comercial.repository.UsuarioRepository;
 
 import javafx.fxml.FXML;
@@ -14,12 +16,18 @@ public class LoginViewController {
 	private UsuarioRepository repository;
 	
 	@FXML
+	private JFXTextField tfUsuario;
+	
+	@FXML
+	private JFXPasswordField pfSenha;
+	
+	@FXML
 	public void handleSair() {
 		System.exit(0);
 	}
 	
 	@FXML
 	public void handleLogin() {
-		System.out.println(repository.findById(1).get().getNome());
+		// Not used
 	}
 }
