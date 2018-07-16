d4a061517b3191d4af0c24e052e4bc6f4b0d2504
falcao998 <felipesouza_analista@yahoo.com.br>
2018-07-16 01:01:25 -0300

    JPA

--- src/main/java/com/sys/automacao/comercial/dao/repository/UsuarioRepository.java
+++ src/main/java/com/sys/automacao/comercial/dao/repository/UsuarioRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import com.sys.automacao.comercial.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Integer>, JpaSpecificationExecutor<Usuario> {
-
+	public Usuario findByUserAndSenha(String user,String senha);
 }
--- src/main/java/com/sys/automacao/comercial/dao/service/UsuarioDaoService.java
+++ src/main/java/com/sys/automacao/comercial/dao/service/UsuarioDaoService.java
@@ -19,4 +19,9 @@ public class UsuarioDaoService {
 	public Optional<Usuario> permissao(String user, String senha) {
 		return repository.findOne(Specification.where(UsuarioSpecification.user(user)).and(UsuarioSpecification.senha(senha)));
 	}
+	
+	//Teste alterenativo
+	public Usuario permissao2(String user, String senha) {
+		return repository.findByUserAndSenha(user,senha);
+	}
 }
