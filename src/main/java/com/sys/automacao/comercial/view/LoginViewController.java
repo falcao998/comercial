34d921392acf59611614fe837f625ec8285c607a
falcao998 <felipesouza_analista@yahoo.com.br>
2018-07-17 00:00:37 -0300

    TELA PRINCIPAL

--- src/main/java/com/sys/automacao/comercial/view/LoginViewController.java
+++ src/main/java/com/sys/automacao/comercial/view/LoginViewController.java
@@ -1,5 +1,7 @@
 package com.sys.automacao.comercial.view;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -9,13 +11,22 @@ import com.sys.automacao.comercial.dao.service.UsuarioDaoService;
 import com.sys.automacao.comercial.model.Usuario;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
 
 @Component
 public class LoginViewController {
 	
 	@Autowired
 	private UsuarioDaoService service;
+	
+	@FXML
+	private AnchorPane anchorPaneLogin;
 	
 	@FXML
 	private JFXTextField tfUsuario;
@@ -44,13 +55,29 @@ public class LoginViewController {
 			if (usuario == null)
 				showMenssagemError();
 			else
-				testeEmptyUsuarioSenha();
+				sucessLogin();
 		}
 	}
 	
 	@FXML
 	public void handleDigitar() {
 		testeEmptyUsuarioSenha();
+	}
+	
+	public void sucessLogin() {
+		try {
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Principal.fxml"));
+			Parent root = fxmlLoader.load();
+			Scene scene = new Scene(root);
+			Stage stage = (Stage)anchorPaneLogin.getScene().getWindow();
+			stage.setScene(scene);
+			stage.centerOnScreen(); 
+			stage.setHeight(Screen.getPrimary().getVisualBounds().getHeight()); 
+			stage.setWidth(Screen.getPrimary().getVisualBounds().getWidth()); 
+			stage.show();
+		}catch(IOException e) {
+			
+		}
 	}
 	
 	private boolean testeEmptyUsuarioSenha() {
